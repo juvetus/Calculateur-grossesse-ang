68c6e75fc7c0b7c63ec1d850936127086705fb19
Juvet <juvettaj@gmail.com>
2023-03-07 11:34:02 +0100

    second commit

--- src/app/calculateur-grossesse/calculateur-grossesse.component.ts
+++ src/app/calculateur-grossesse/calculateur-grossesse.component.ts
@@ -1,28 +1,53 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-calculateur-grossesse',
   templateUrl: './calculateur-grossesse.component.html',
-  styleUrls: ['./calculateur-grossesse.component.css']
+  styleUrls: ['./calculateur-grossesse.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('0.5s ease-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class CalculateurGrossesseComponent {
 
   ddr!: Date;
   cycleMoyen?: number;
   betaHCG?: number;
-  ageGestationnel?: string;
-  dateAccouchement?: string;
+  ageGestationnel!: string;
+  dateAccouchement!: string | null;
+  enceinte!: boolean;
+
+
+
+
+
+  constructor(private datePipe: DatePipe) {}
 
   calculer() {
-    // Calculer l'âge gestationnel
-    const joursEcoules = (Date.now() - new Date(this.ddr).getTime()) / 86400000;
+
+
+    const ddrDate = new Date(this.ddr);
+    const joursEcoules = (Date.now() - ddrDate.getTime()) / 86400000;
     const semainesGrossesse = Math.floor(joursEcoules / 7);
     const joursRestants = Math.floor(joursEcoules % 7);
     this.ageGestationnel = `${semainesGrossesse} semaine(s) et ${joursRestants} jour(s)`;
 
-    // Estimer la date d'accouchement probable
-    const ddrPlus9Mois = new Date(this.ddr);
-    ddrPlus9Mois.setDate(ddrPlus9Mois.getDate() + 280);
-    this.dateAccouchement = ddrPlus9Mois.toLocaleDateString();
+    const dateAccouchementDate = new Date(ddrDate.getTime() + (280 * 86400000));
+    this.dateAccouchement= this.datePipe.transform(dateAccouchementDate, 'dd/MM/yyyy');
+
+    this.enceinte = semainesGrossesse >= 4;
   }
+
+
+
 }
